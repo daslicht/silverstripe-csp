8fc439e9136fb521a2206ad47ffd31bdf47f634d
zauberfisch <admin@zauberfisch.at>
2014-06-23 22:40:39 +0000

    use _ss_environment.php for environment configuration

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,15 +3,15 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => '127.0.0.1',
-	"username" => 'root',
-	"password" => '',
-	"database" => 'SS_capesideport',
-	"path" => '',
-);
+// use _ss_environment.php for DB config
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
+
+if (Director::isLive()) {
+	// we are in live mode, send errors per email
+	SS_Log::add_writer(new SS_LogEmailWriter('myEmail@mysite.com'), SS_Log::ERR);
+	// IMPORTANT: as of 3.1 you can *NOT* set display_errors inside _config.php
+	// use the php ini, htaccess or _ss_environment.php to set display_errors
+}
